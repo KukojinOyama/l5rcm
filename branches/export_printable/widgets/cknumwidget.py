137ecd1daa5f924dc61a6cd460aeccf5c94ef407
oppifjellet@gmail.com <oppifjellet@gmail.com@7cbe2a70-d7a5-354d-e4e2-545967246cb4>
2011-10-23 15:51:30 +0000

    merged back some changes in truck

--- branches/export_printable/widgets/cknumwidget.py
+++ branches/export_printable/widgets/cknumwidget.py
@@ -30,56 +30,58 @@ class CkNumWidget(QtGui.QWidget):
 
     def __init__(self, count = 9, parent = None):
         super(CkNumWidget, self).__init__(parent)
-        
+
+        self.count  = count
         self.checks = []
-        self.count = count
-        self.value = 0
+        self.value  = 0
         hbox = QtGui.QHBoxLayout(self)
         hbox.setSpacing(0)
-        hbox.setContentsMargins(0,0,0,0)        
+        hbox.setContentsMargins(0,0,0,0)
         for i in xrange(0, count):
             ck = QtGui.QCheckBox(self)
             self.checks.append( ck )
             hbox.addWidget( ck )
             ck.clicked.connect(self.on_ck_toggled)
-            ck.setObjectName( str(i) )
+            ck.setObjectName( str(i+1) )
 
     def on_ck_toggled(self):
         old_v = self.value
         fred = find(lambda ck: ck == self.sender(), self.checks)
         flag = fred.isChecked()
-        cnt = self.value = int(fred.objectName())
-        if not flag:
-            cnt -= 1
+        
+        if int(fred.objectName()) == old_v:
+            self.value = self.value - 1
+        else:
+            self.value = int(fred.objectName())
+                
+        #print 'old_v: %d, value: %d' % (old_v, self.value)
+         
         for i in xrange(0, self.count):
             ck = self.checks[i]
             if flag:
-                if int(ck.objectName()) <= cnt:
+                if int(ck.objectName()) <= self.value:
                     self.checks[i].setChecked(flag)
                 else:
                     self.checks[i].setChecked(not flag)
             else:
-                if int(ck.objectName()) <= cnt:
+                if int(ck.objectName()) <= self.value:
                     self.checks[i].setChecked(not flag)
                 else:
                     self.checks[i].setChecked(flag)
-        
-        print 'old_v: %d, cur_v: %d' % (old_v, self.value)                    
         if self.value != old_v:
             self.valueChanged.emit(old_v, self.value)
 
     def set_value(self, value):
         if value == self.value:
             return
-        
         for i in xrange(0, self.count):
             ck = self.checks[i]
-            if int(ck.objectName()) < value:
+            if int(ck.objectName()) <= value:
                 self.checks[i].setChecked(True)
             else:
                 self.checks[i].setChecked(False)
         old_v = self.value
-        self.value = value                
+        self.value = value
 
         self.valueChanged.emit(old_v, value)
 
