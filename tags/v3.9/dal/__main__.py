00ebb099872326348cd7de7ab276373f42892e9d
oppifjellet@gmail.com <oppifjellet@gmail.com@7cbe2a70-d7a5-354d-e4e2-545967246cb4>
2013-06-28 06:33:10 +0000

    Tagged 3.9 version

---
+++ tags/v3.9/dal/__main__.py
@@ -0,0 +1,7 @@
+
+dal = Data("share/l5rcm/data")
+print('# clans:    {0}'.format(len(dal.clans   )))
+print('# families: {0}'.format(len(dal.families)))
+print('# schools:  {0}'.format(len(dal.schools )))
+
+
---
+++ tags/v3.9/pylint.bat
@@ -0,0 +1,7 @@
+call c:\python27\scripts\pylint -E --rcfile pylint.rc l5rcm.py
+call c:\python27\scripts\pylint -E --rcfile pylint.rc l5rcmcore.py
+call c:\python27\scripts\pylint -E --rcfile pylint.rc osutil.py
+call c:\python27\scripts\pylint -E --rcfile pylint.rc dal
+call c:\python27\scripts\pylint -E --rcfile pylint.rc dialogs
+call c:\python27\scripts\pylint -E --rcfile pylint.rc models
+call c:\python27\scripts\pylint -E --rcfile pylint.rc sinks
