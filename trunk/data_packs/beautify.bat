24e9790bd2987128c417cf854efc6a45be1fa376
oppifjellet@gmail.com <oppifjellet@gmail.com@7cbe2a70-d7a5-354d-e4e2-545967246cb4>
2013-07-24 16:37:02 +0000

    add tech descriptions beautifier

--- trunk/data_packs/beautify.bat
+++ trunk/data_packs/beautify.bat
@@ -1,1 +1,2 @@
-for /r %%i in (*.xml) do xml fo --indent-spaces 2 --encode utf-8 "%%i" > "%%i"
+for /r %%i in (*.xml) do xml fo --indent-spaces 2 --encode utf-8 "%%i" > tmp.xml && copy tmp.xml "%%i"
+
